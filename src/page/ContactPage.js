78ab5df41de0817b70ebe707a1bb2679279b751b
Jun <junliang1115@gmail.com>
2025-10-05 01:20:42 +0800

    change fetch local api to render api

--- src/page/ContactPage.js
+++ src/page/ContactPage.js
@@ -15,7 +15,7 @@ function ContactPage() {
         e.preventDefault();
         setLoading(true);
         try {
-            const res = await fetch('/api/contact', {
+            const res = await fetch('https://junliang-portfolio-backend.onrender.com/api/contact', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({
