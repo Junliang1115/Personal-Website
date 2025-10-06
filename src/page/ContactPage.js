b6e86323172a12f74b820b5647161ff44b8b7619
Jun <junliang1115@gmail.com>
2025-10-07 01:11:15 +0800

    debug msg sending issue

--- src/page/ContactPage.js
+++ src/page/ContactPage.js
@@ -24,10 +24,8 @@ function ContactPage() {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({
-                    firstName,
-                    lastName,
+                    name: `${firstName} ${lastName}`.trim(),
                     email,
-                    phoneNumber,
                     message,
                 }),
             });
